06ab7aec703fd555d65be508ca93c2bf7f3f5fb9
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2022-11-04 19:41:20 +0000

    DELETE test

--- app.js
+++ app.js
@@ -20,12 +20,14 @@ app.get("/api/users/:id", userHandlers.getUsersById);
 app.get("/api/users", userHandlers.getUsers);
 app.post("/api/users", userHandlers.postUser);
 app.put("/api/users/:id", userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 
 //app.post("/api/movies", movieHandlers.postMovie);
 //app.get("/api/movies", movieHandlers.getMovies);
 //app.get("/api/movies/:id", movieHandlers.getMovieById);
 //app.put("/api/movies/:id", movieHandlers.updateMovie);
+//app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 app.listen(port, (err) => {
   if (err) {
