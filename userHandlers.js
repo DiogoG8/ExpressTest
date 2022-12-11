14fb7179c9c79b4a9f431adad4eac965dd739a3f
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2022-12-11 07:49:18 +0000

    Changes on route GetByID

--- userHandlers.js
+++ userHandlers.js
@@ -45,7 +45,7 @@ const getUsersById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query("select * from users where id = ?", [id])
+    .query("select id, firstname, lastname, email, city, language from users where id = ?", [id])
     .then(([users]) => {
       if (users[0] != null) {
         res.status(200).json(users[0]);
